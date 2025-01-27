5cc2f40e87fd5ee8c9a12740900217bcdf556627
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-27 23:53:55 +0530

    txt

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import Call from './component/call'
 
 const App = () => {
   return (
-    // <div className='flex justify-center m-1 p-2 items-center gap-1' >
-    //   <p className='text-2xl text-red-600'>hello</p>
-    //   <p>i am coder</p>
-    //   <Call />
-    // </div>
+
     <div className='flex justify-center items-center gap-1'>
       <h1 class="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <Call/>
+      <Call />
     </div>
   )
 }
--- src/component/Call.jsx
+++ src/component/Call.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Call = () => {
   return (
     <div>
-      call me
+      <h1 class="text-3xl font-bold text-red-600">call me</h1>
     </div>
   )
 }
