a5f974fb762fa819ba8e0cde0613f97cc8ea6715
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-27 23:42:49 +0530

    txt

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Call from './component/call'
+
+const App = () => {
+  return (
+    // <div className='flex justify-center m-1 p-2 items-center gap-1' >
+    //   <p className='text-2xl text-red-600'>hello</p>
+    //   <p>i am coder</p>
+    //   <Call />
+    // </div>
+    <div className='flex justify-center'>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Call/>
+    </div>
+  )
+}
+
+export default App
