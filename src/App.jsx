9ec362418298e115b19ba6f02694edccb1958500
chaitanyakumar99 <gchkumar999@gmail.com>
2025-02-08 20:12:05 +0530

    txt

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Call from './component/call'
 const App = () => {
   return (
 
-    <div className='flex justify-center items-center gap-1 bg-black text-white'>
-      <h1 class="text-3xl font-bold underline">
+    <div className=''>
+      <h1 class="flex justify-center items-center gap-1 bg-black text-white text-3xl font-bold underline">
         Hello world!
       </h1>
       <Call />
+
     </div>
   )
-  
+
 }
 
 export default App
+
+
--- src/component/Call.jsx
+++ src/component/Call.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Call = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold text-red-600">call me</h1>
+      <h1 class="text-3xl font-bold text-white mb-6000  bg-black">Welcome to the world </h1>
     </div>
   )
 }
